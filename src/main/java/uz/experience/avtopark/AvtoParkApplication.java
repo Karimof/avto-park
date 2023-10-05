b384bfdfb682feba7900787468ef20deb57a5b68
Karimof <islamkarimov8580@gmail.com>
2023-10-05 20:33:06 +0500

    Initial commit

---
+++ src/main/java/uz/experience/avtopark/AvtoParkApplication.java
@@ -0,0 +1,13 @@
+package uz.experience.avtopark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvtoParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvtoParkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/experience/avtopark/AvtoParkApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.experience.avtopark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvtoParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
